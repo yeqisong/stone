fdb16c115d497e4984efd0ac4cdef3e48065aeec
yeqisong <445287302@qq.com>
2026-07-07 20:30:01 +0800

    fix: FeatureDetail 编辑弹窗缺少 NModal/NSpace 导入

--- web-v2/src/components/FeatureDetail.vue
+++ web-v2/src/components/FeatureDetail.vue
@@ -139,7 +139,7 @@
 
 <script setup>
 import { ref, computed, onMounted, nextTick, watch } from 'vue'
-import { NButton, NTag, NSpin, NTabs, NTabPane, NInput, NDataTable, NEmpty, NPagination } from 'naive-ui'
+import { NButton, NTag, NSpin, NTabs, NTabPane, NInput, NDataTable, NEmpty, NPagination, NModal, NSpace } from 'naive-ui'
 import axios from 'axios'
 import * as echarts from 'echarts'
 
